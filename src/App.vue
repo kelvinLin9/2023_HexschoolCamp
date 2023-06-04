d23f2d11bd3161f6bd4ef261febdfa3067824389
kelvinLin9 <kelvin80121@hotmail.com>
2023-06-04 11:37:51 +0800

    部分完成存檔

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,9 @@
 <template>
-  <div class="bg-full">
-    <router-view/>
+  <div class="bg-full bg-dark d-flex justify-content-center align-items-center">
+    <router-view class="d-flex flex-column align-items-center"></router-view>
   </div>
 </template>
 
 <style lang="scss">
 @import "./assets/scss/all.scss";
-body {
-  overflow-x: hidden;
-  cursor: url(./assets/images/sym761.ani);
-}
-::-webkit-scrollbar {
-    width: 8px;
-}
-
-::-webkit-scrollbar-track {
-    --webkit-box-shadow: inset 0 0 6px rgba(0,0,0,0.3);
-    border-radius: 6px;
-}
-
-::-webkit-scrollbar-thumb {
-    border-radius: 3px;
-    background: #38241B;
-    --webkit-box-shadow: inset 0 0 3px rgba(0,0,0,0.5);
-}
-
-::-webkit-scrollbar-thumb:window-inactive {
-    background: #f8f8f8;
-}
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,76 +1,175 @@
 <template>
   <div>
-    <section class="header">
+    <section class="header d-flex flex-column justify-content-center align-items-center test">
       <!-- navbar -->
-      <nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-d             ark">
-        <div class="container-fluid">
-          <a class="navbar-brand ps-5" href="#">
-            <img src="https://raw.githubusercontent.com/hexschool/2022-web-layout-training/main/2023web-camp/logo.png" alt="AI工具王">
-          </a>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav ms-auto mb-2 mb-lg-0 pe-5">
-              <li class="nav-item">
-                <a class="nav-link fs-18 text-white" href="#"
-                @click.prevent="moveToChallenge()"
-                >
-                  關卡任務
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link fs-18 text-white" href="#"
-                @click.prevent="moveToInstructions()"
-                >
-                  競賽說明
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link fs-18 text-white bg-danger rounded-pill px-5" href="https://2022.thef2e.com/users">立即報名</a>
-              </li>
-            </ul>
+      <div class="navbar">
+        <div class="logo">
+          <img src="https://raw.githubusercontent.com/hexschool/2022-web-layout-training/main/2023web-camp/logo.png" alt="AI工具王"/>
+        </div>
+        <span class="material-symbols-outlined d-flex align-center menu">menu</span>
+        <ul class="d-flex align-center bold lh-3">
+          <li>
+            <a href="#" class="py-12">首頁</a>
+          </li>
+          <li>
+            <a href="#" class="py-12">定價</a>
+          </li>
+        </ul>
+      </div>
+      <div class="home-slogan Noto-Sans-TC-900 text-white fs-120 test">
+        <div>透過</div>
+        <div>AI 工具王的</div>
+        <div>強大模型</div>
+        <div>讓您的業務更聰明</div>
+      </div>
+      <div>
+        <button type="button" class="start-use btn btn-dark text-white fs-20">
+          開始使用
+        </button>
+      </div>
+    </section>
+    <section class="serve container my-5 test">
+      <div class="row my-5">
+        <div class="col-4 Noto-Sans-TC-900 text-white fs-32 me-auto">
+          我們的服務
+        </div>
+        <div class="col-8 Noto-Sans-TC-900 text-white fs-48">
+          我們相信透過 AI 技術，<br>
+          包括圖像識別、自然語言處理和機器翻<br>譯等，讓您更快、更準確地做出決策。
+        </div>
+      </div>
+      <div class="row my-5">
+        <div class="col-4 sever-feature">
+          <img src="../assets/images/Vector.png" alt="服務logo">
+          <h4 class="Noto-Sans-TC-900 text-white fs-32">快速</h4>
+          <h5 class="Noto-Sans-TC-400 text-white fs-16">我們先進的推理基礎設施提供了極短的響應時<br>間，讓你的產品服務不間斷。</h5>
+        </div>
+        <div class="col-4 sever-feature">
+          <img src="../assets/images/Vector-1.png" alt="服務logo">
+          <h4 class="Noto-Sans-TC-900 text-white fs-32">靈活</h4>
+          <h5 class="Noto-Sans-TC-400 text-white fs-16">我們的服務可以根據您的需求進行靈活調整，<br> 同時還可以滿足不同項目和預算的需求。</h5>
+        </div>
+        <div class="col-4 sever-feature">
+          <img src="../assets/images/Vector-2.png" alt="服務logo">
+          <h4 class="Noto-Sans-TC-900 text-white fs-32">擴充</h4>
+          <h5 class="Noto-Sans-TC-400 text-white fs-16">我們的服務支持多種擴充選項，可以滿足您的<br>不斷增長的 AI 需求。</h5>
+        </div>
+      </div>
+    </section>
+    <section class="container my-5 test">
+      <div class="row">
+        <div class="col-4 evaluate">
+          <div class="evaluate-star py-3">
+            <img src="../assets/images/Star.png" alt="">
+            <img src="../assets/images/Star.png" alt="">
+            <img src="../assets/images/Star.png" alt="">
+            <img src="../assets/images/Star.png" alt="">
+            <img src="../assets/images/Star.png" alt="">
+          </div>
+          <div>
+            <span class="Noto-Sans-TC-400 text-white fs-16">
+              非常喜歡 AI工具王 提供的 AI 模型租賃服務，使<br>用起來非常方便，而且效果非常好。對於我們<br>
+              公司的業務來說，這個服務非常重要，因為我<br>們需要大量的數據進行分析和預測，AI工具王<br>
+              提供的 AI 模型能夠幫助我們節省大量時間和成<br>本，實在是太棒了！
+            </span>
+          </div>
+          <div class="row">
+            <div class="col-2">
+              <img src="../assets/images/Ellipse3.png" alt="">
+            </div>
+            <div class="col">
+              <div class="Noto-Sans-TC-400 text-white fs-16">陳小姐</div>
+              <div class="Noto-Sans-TC-400 text-white fs-14">ABC科技有限公司</div>
+            </div>
           </div>
         </div>
-      </nav>
-<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+        <div class="col-4 evaluate">
+          <div class="evaluate-star py-3">
+            <img src="../assets/images/Star.png" alt="">
+            <img src="../assets/images/Star.png" alt="">
+            <img src="../assets/images/Star.png" alt="">
+            <img src="../assets/images/Star.png" alt="">
+            <img src="../assets/images/Star.png" alt="">
+          </div>
+          <div>
+            <span class="Noto-Sans-TC-400 text-white fs-16">
+              非常喜歡 AI工具王 提供的 AI 模型租賃服務，使<br>用起來非常方便，而且效果非常好。對於我們<br>
+              公司的業務來說，這個服務非常重要，因為我<br>們需要大量的數據進行分析和預測，AI工具王<br>
+              提供的 AI 模型能夠幫助我們節省大量時間和成<br>本，實在是太棒了！
+            </span>
+          </div>
+          <div class="row">
+            <div class="col-2">
+              <img src="../assets/images/Ellipse3.png" alt="">
+            </div>
+            <div class="col">
+              <div class="Noto-Sans-TC-400 text-white fs-16">陳小姐</div>
+              <div class="Noto-Sans-TC-400 text-white fs-14">ABC科技有限公司</div>
+            </div>
+          </div>
+        </div>
+        <div class="col-4 evaluate">
+          <div class="evaluate-star py-3">
+            <img src="../assets/images/Star.png" alt="">
+            <img src="../assets/images/Star.png" alt="">
+            <img src="../assets/images/Star.png" alt="">
+            <img src="../assets/images/Star.png" alt="">
+            <img src="../assets/images/Star.png" alt="">
+          </div>
+          <div>
+            <span class="Noto-Sans-TC-400 text-white fs-16">
+              非常喜歡 AI工具王 提供的 AI 模型租賃服務，使<br>用起來非常方便，而且效果非常好。對於我們<br>
+              公司的業務來說，這個服務非常重要，因為我<br>們需要大量的數據進行分析和預測，AI工具王<br>
+              提供的 AI 模型能夠幫助我們節省大量時間和成<br>本，實在是太棒了！
+            </span>
+          </div>
+          <div class="row">
+            <div class="col-2">
+              <img src="../assets/images/Ellipse3.png" alt="">
+            </div>
+            <div class="col">
+              <div class="Noto-Sans-TC-400 text-white fs-16">陳小姐</div>
+              <div class="Noto-Sans-TC-400 text-white fs-14">ABC科技有限公司</div>
+            </div>
+          </div>
+        </div>
+      </div>
     </section>
 
 
-
-    <footer>
-      <div class="bg-white d-flex justify-content-center py-3">
-        <a href="https://titansoft.com/tw/go_jira">
-          <img src="../assets/images/jira.png" alt="jira-logo" class="px-5">
-        </a>
-        <a href="https://www.atlassian.com/software/confluence">
-          <img src="../assets/images/confluence.png" alt="confluence-logo" class="px-5">
-        </a>
-        <a href="https://titansoft.com/tw/get_miro">
-          <img src="../assets/images/miro.png" alt="miro-logo" class="px-5">
-        </a>
-        <a href="https://www.dottedsign.com/zh-tw?utm_source_k=PR&utm_campaign_k=PR_Hexschool_SponsorEvent_2022&utm_medium_k=Hexschool">
-          <img src="../assets/images/dotted.png" alt="dotted-logo" class="px-5">
-        </a>
-      </div>
-      <div class="bg-primary">
-        <h2 class="Noto-Serif-TC text-white text-center pt-5">贊助單位</h2>
-        <div class="d-flex justify-content-around py-5">
-          <a href="https://blockstudio.tw/">
-            <img src="../assets/images/blockstudio.png" alt="blockstudio-logo">
-          </a>
-          <a href="https://www.kdanmobile.com/zh-tw">
-            <img src="../assets/images/kdan.png" alt="kdan-logo">
-          </a>
-          <a href="https://titansoft.com/tw">
-            <img src="../assets/images/titan.png" alt="titan-logo">
-          </a>
+    <section class="">
+      <h3 class=" Noto-Sans-TC-900 text-white fs-48 text-center">
+        現在就來建立屬於你的服務吧
+      </h3>
+      <button type="button" class="btn btn-dark  Noto-Sans-TC-900 text-white fs-20">
+        開始使用
+        <img src="../assets/images/Vector-5.png" alt="">
+      </button>
+    </section>
+    <footer class="footer">
+      <div class="footer-top d-flex justify-content-between py-5">
+        <div>
+          <h2><a href="#" class="Noto-Sans-TC-900 text-white fs-80 text-decoration-none">首頁</a></h2>
+          <h2><a href="#" class="Noto-Sans-TC-900 text-white fs-80 text-decoration-none">定價</a></h2>
         </div>
-        <div class="text-white text-center Noto-Serif-TC pb-3">
-          六角日報 © Code: <a href="https://github.com/kelvinLin9" class="text-danger text-decoration-none">Kelvin Lin</a>   /  Design: <a href="https://www.behance.net/KT_Designer" class="text-danger text-decoration-none">KT</a>
+        <div class="">
+          <img src="https://raw.githubusercontent.com/hexschool/2022-web-layout-training/main/2023web-camp/logo.png" alt="logo">
         </div>
       </div>
-    </footer>
+      <div class="footer-section d-flex justify-content-between">
+        <div class="d-flex py-5">
+          <h5 class="Noto-Sans-TC-900 text-white fs-16 text-decoration-none pe-5">AI工具王 © 2023</h5>
+          <ul class="list-group list-group-horizontal">
+            <li><a href="#" class="Noto-Sans-TC-900 text-white fs-16 text-decoration-none px-2">Facebook</a></li>
+            <li><a href="#" class="Noto-Sans-TC-900 text-white fs-16 text-decoration-none px-2">Twitter</a></li>
+            <li><a href="#" class="Noto-Sans-TC-900 text-white fs-16 text-decoration-none px-2">Instagram</a></li>
+          </ul>
+        </div>
+        <div class="py-5">
+          <p class="Noto-Sans-TC-900 text-white fs-16 text-decoration-none px-2">Back to top</p>
+        </div>
+      </div>
+    </footer> 
   </div>
 
 </template>
@@ -144,5 +243,54 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+// header
+.header {
+  width: 1920px;
+  height: 1080px;
+}
+.navbar {
+  width: 1296px;
+  height: 80px;
+  border-bottom: solid 1px white;
+}
+.logo {
+  width: 20px;
+  height: 40px;
+}
+.home-slogan {
+  width: 960px;
+  height: 576px;
+  line-height: 120%;
+}
+.start-use {
 
+}
+//服務
+.serve{
+  // width: 1296px;
+  // height: 620px;
+}
+.sever-feature {
+  border: 1px solid #525252;
+  border-radius: 16px;
+  img {
+    padding: 86.67px 0 26.67px 40px;
+  }
+  h4 {
+    padding: 0 0 20px 40px;
+  }
+  h5 {
+    padding: 0 0 80px 40px;
+  }
+}
+// 評價
+.evaluate {
+background: rgba(255, 255, 255, 0.08);
+  box-shadow: 0px 100px 80px rgba(18, 18, 18, 0.07), 0px 41.7776px 33.4221px rgba(18, 18, 18, 0.0503198), 0px 22.3363px 17.869px rgba(18, 18, 18, 0.0417275), 0px 12.5216px 10.0172px rgba(18, 18, 18, 0.035), 0px 6.6501px 5.32008px rgba(18, 18, 18, 0.0282725), 0px 2.76726px 2.21381px rgba(18, 18, 18, 0.0196802);
+  border-radius: 16px;
+}
+// footer
+.footer-top {
+  border-bottom: solid 1px white;
+}
 </style>
