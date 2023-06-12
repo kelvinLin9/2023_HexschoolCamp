edaf7badb78c487a9b569d847635484a11b9cb41
kelvinLin9 <kelvin80121@hotmail.com>
2023-06-12 21:30:47 +0800

    調整卷軸樣式

--- src/App.vue
+++ src/App.vue
@@ -6,4 +6,26 @@
 
 <style lang="scss">
 @import "./assets/scss/all.scss";
+/* Width */
+::-webkit-scrollbar {
+  width: 5px;
+  height: 5px;
+}
+
+/* Track */
+::-webkit-scrollbar-track {
+  background: #f1f1f1;
+  border-radius: 10px;
+}
+
+/* Handle */
+::-webkit-scrollbar-thumb {
+  background: #888;
+  border-radius: 10px;
+}
+
+/* Handle on hover */
+::-webkit-scrollbar-thumb:hover {
+  background: #555;
+}
 </style>
